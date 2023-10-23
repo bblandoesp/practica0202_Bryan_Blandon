6b4fb07bdb79b5c329237c1646a15ae27946a820
bblandoesp <bblandoesp@educacion.navarra.es>
2023-10-23 22:06:56 +0200

    Tributar

---
+++ Ejercicio 4.py
@@ -0,0 +1,8 @@
+e = int (input ("Edad:"))
+i_m = float (input ("Ingresos mensuales:"))
+if e >= 16: 
+    if i_m >= 1000:
+         print ("¡tiene que tributar!")
+else:
+     print ("¡No tiene que tributar!")
+print ("¡No tiene que tributar!")
