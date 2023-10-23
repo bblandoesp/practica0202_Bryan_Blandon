1e12c401a1e0d3ee97b32b0dc816b09be34a4b4f
bblandoesp <bblandoesp@educacion.navarra.es>
2023-10-23 20:07:00 +0200

    División

---
+++ Ejercicio 2.py
@@ -0,0 +1,6 @@
+n1 = int (input ("numero:")) #dividendo 
+n2 = int (input ("numero:")) #divisor
+if n2 == 0:
+    print ("¡Error!")
+elif n2 != 0:
+    print ("El resultado de la division es:", n1/n2)
